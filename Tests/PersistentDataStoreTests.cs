a0391133d324ee5200e0b8627090dec2e11d529b
DUBITLIMITED\david.smithson <david.smithson@dubitlimited.com>
2018-06-07 15:24:42 +0100

    Setup

---
+++ Tests/PersistentDataStoreTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using DUCK.PersistentDataStore;
+
+namespace DUCK.PersistentDataStore.Tests
+{
+	public class PersistentDataStoreTests
+	{
+		[Test]
+		public void ExpectNothing()
+		{
+
+		}
+	}
+}
